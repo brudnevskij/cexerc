532a5f78405dce710ff40a05a05f0fe1f97e0358
brudnevskij <forbiddenpass@gmail.com>
2021-12-04 18:01:08 +0200

    2

--- studies.c
+++ studies.c
@@ -18,6 +18,7 @@ int is_substring(char s1[], char s2[]){
     if(dif >= 0){
 	for(int i = 0; i <= dif; i++){
 	    for(int j = 0; j < sublen; j++){
+		printf("%c==%c\n", s2[i+j], s1[j]);
 		if(s2[i+j] == s1[j]) counter++;
 		if(counter == sublen){
 		    return 1;
@@ -29,6 +30,41 @@ int is_substring(char s1[], char s2[]){
     
     return 0;
 }
+
+
+int is_substring_mod(char s1[], char s2[]){
+    int sublen = stringlen(s1);
+    int strlen = stringlen(s2);
+    int dif = strlen - sublen;
+    int counter = 0;
+    if(dif >= 0 ){
+	for(int i = 0; i<= dif; i++){
+	    if(s1[0] == s2[i]){
+		for(int j = 1; j < sublen; j++){
+		    printf("%c==%c\n", s2[i+j], s1[j]);
+		    if(s1[j] == s2[i+j]){
+			 counter++;
+		    }else{
+			 break;
+		    }
+
+		    if(counter == sublen-1){
+			return 1;
+}
+}
+		counter = 0;
+}
+}
+}
+
+
+
+    return 0;
+}
+
+
+
+
 
 int is_beginning(char s1[], char s2[]){
     int counter;
@@ -50,7 +86,8 @@ int is_beginning(char s1[], char s2[]){
 int is_polindrome(char s[]){
     int strlen = stringlen(s);
     int temp = strlen-1;
-    for(int i = 0; i < strlen; i++){
+    for(int i = 0; i < strlen/2+1; i++){
+	printf("s[%d]==s[%d]\n", i, temp);
 	if(s[i] == s[temp]){
 	    temp--;
 	    continue;
@@ -170,15 +207,58 @@ int str_to_int(char s[]){
     return r;
 }
 
+char* int_to_str(int n){
+    int counter = 0;
+    char* str;
+    while(n/(int)pow(10,counter)!= 0){
+	counter++;
+}
+    str = (char*)malloc(counter*sizeof(char)+1);
+    str[counter] = '\0';
+    for(int i = 1; i<=counter; i++){
+	str[counter-i]= n%(int)pow(10,i)/pow(10, i-1)+48;    
+}
+
+    return str;
+}
+
+char* add1(char s1[], char s2[]){
+    int str1len = stringlen(s1);
+    char* r;
+    r = (char*)malloc(str1len*sizeof(char)+1);
+    for(int i =1; i <= str1len;i++){
+	r[str1len-i] =(char)(s1[0]-48)+(s2[0]-48)+48;
+}
+
+    return r;
+}
+
+
+
+
+
+char* add2(char s1[], char s2[]){
+
+
+}
+
 int main(){
 
 //    printf("%d",is_beginning("sss", "sssas"));
-//    printf("%d",is_beginning("sss", "sss"));
+//    printf("%d",is_beginning("sss", "sss"));%
 //    printf("%d",is_beginning("sss", "ssasa"));
-    printf("aaasass %s\n", upcase("aaasass"));
-    printf("asasab %s\n", swap_case("asasab"));
-    printf("aaa %s\n", dupstring("aaa", 3));
-    printf("laaa1233AAA15 %s %d\n", appendr_char("aaa1233AAA15",'l'),(int)pow(10, 2) );
-    printf("%d %d", str_to_int("696969"), '1');
+//    printf("aaasass %s\n", upcase("aaasass"));
+//    printf("asasab %s\n", swap_case("asasab"));
+//    printf("aaa %s\n", dupstring("aaa", 3));
+//    printf("laaa1233AAA15 %s %d\n", appendr_char("aaa1233AAA15",'l'),(int)pow(10, 2) );
+//    printf("%d %d", str_to_int("696969"), '1');
+//    is_polindrome("ojojojojojojojojojojojojojojojo");
+//    is_polindrome("aaa");   
+//    is_polindrome("ojojob");
+//    is_substring("asa", "asbaaaaasa");
+    printf("%s", add1("33","33") );
+//    printf("%d", 124%10/1);
+//    printf("%d\n",is_substring_mod("asa", "ssssssssssssssasa"));
+//    is_substring_mod("asa", "asbaaaaasa");
     return 0;
 }
